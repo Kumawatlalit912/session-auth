784448be55b81879810c033689847d013d802b42
kumawatlalit912 <kumawatlalit912@gmail.com>
2023-04-08 18:16:35 +0530

    added-login-and-register-page-with-auth

--- authPractice.js
+++ authPractice.js
@@ -3,6 +3,8 @@ const app = express();
 const session = require("express-session");
 const mongodbSession = require("connect-mongodb-session")(session);
 const mongoose = require("mongoose");
+const User = require("./models/userModel");
+const bcrypt = require("bcryptjs");
 const port = 3000;
 const mongoUri =
   "mongodb+srv://manage:1234@cluster0.863eudz.mongodb.net/?retryWrites=true&w=majority";
@@ -37,6 +39,36 @@ app.use(
 app.get("/", (req, res) => {
   console.log(req.session.id);
   res.send("hello world");
+});
+app.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  const passkey = await bcrypt.hash(password, 12);
+  const isExists = await User.findOne({ email });
+  if (isExists) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+  const user = await User.create({
+    username,
+    email,
+    password: passkey,
+  });
+  await user.save();
+  redirect("/login");
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      req.session.isAuth = true;
+      redirect("/dashboard");
+      return res.status(200).json({ message: "Login successful" });
+    }
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+  return res.status(400).json({ message: "Invalid credentials" });
 });
 
 app.listen(port, () => {
